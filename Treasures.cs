39f182ed2c3e076d86146d1e64d62bc3671cc0fb
xiaowu96 <826264309@qq.com>
2017-09-25 16:10:29 +0800

    v1
    ein_v

---
+++ Treasures.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Treasures : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter(Collision collisionInfo)
+	{
+		if (collisionInfo.collider.tag == "myBoat")
+		{
+			Destroy(gameObject);
+		}
+
+	}
+}
